6b46e27b62a84802c95803455c1b389eb1e928c9
John Bachman <bachmanjohn@gmail.com>
2017-10-24 11:09:43 -0400

    Get correlated genes from RNA

--- cptac/sc3/process_brca_data.py
+++ cptac/sc3/process_brca_data.py
@@ -1,7 +1,9 @@
 import pickle
 import numpy as np
+from collections import Counter
 from indra.util import read_unicode_csv
 import scipy.stats
+
 
 cell_lines = [
  'BT20',
@@ -35,7 +37,7 @@ cell_lines = [
  'HCC1143',
  'HCC1428',
  'HME1',
- 'MCF10AREP2',
+ #'MCF10AREP2',
  'SUM1315',
  'SUM149',
  'SUM159',
@@ -43,7 +45,7 @@ cell_lines = [
 ]
 
 
-def get_cell_line_col_maps(pms_filename, ibaq_filename):
+def get_cell_line_col_maps(pms_filename, ibaq_filename, rna_filename):
     pms_col_map = {}
     with open(pms_filename, 'rt') as f:
         pms_header = f.readline().strip().split(',')
@@ -59,18 +61,30 @@ def get_cell_line_col_maps(pms_filename, ibaq_filename):
             capcol = col.upper()
             if capcol in cell_lines:
                 ibaq_col_map[capcol] = col_ix
-    return (pms_col_map, ibaq_col_map)
+
+    rna_col_map = {}
+    with open(rna_filename, 'rt') as f:
+        rna_header = f.readline().strip().split('\t')
+        for col_ix, col in enumerate(rna_header):
+            capcol = col.upper()
+            if capcol in cell_lines:
+                rna_col_map[capcol] = col_ix
+
+    return (pms_col_map, ibaq_col_map, rna_col_map)
 
 
-def load_data(pms_filename, ibaq_filename):
+def load_data(pms_filename, ibaq_filename, rna_filename):
+    pms_col_map, ibaq_col_map, rna_col_map = \
+            get_cell_line_col_maps(pms_filename, ibaq_filename, rna_filename)
     # Get the ibaq data first
     prot_labels = []
     prot_rows = []
     for row_ix, row in enumerate(read_unicode_csv(ibaq_filename, skiprows=1)):
         values = []
         gene_name = row[0]
-        uniprot_id = row[-3]
-        site_position = row[-5]
+        if gene_name == '0':
+            gene_name = None
+        uniprot_id = row[1]
         for cell_line in cell_lines:
             col_ix = ibaq_col_map[cell_line]
             if not row[col_ix]:
@@ -80,7 +94,7 @@ def load_data(pms_filename, ibaq_filename):
             float(val)
             values.append(val)
         values = np.array(values)
-        prot_labels.append((gene_name, uniprot_id, site_position))
+        prot_labels.append((gene_name, uniprot_id))
         prot_rows.append(values)
 
     # Then get the phospho-MS data
@@ -89,6 +103,8 @@ def load_data(pms_filename, ibaq_filename):
     for row_ix, row in enumerate(read_unicode_csv(pms_filename, skiprows=1)):
         values = []
         site_name = row[0]
+        uniprot_id = row[-3]
+        site_position = row[-5]
         for cell_line in cell_lines:
             col_ix = pms_col_map[cell_line]
             if not row[col_ix]:
@@ -98,30 +114,97 @@ def load_data(pms_filename, ibaq_filename):
             float(val)
             values.append(val)
         values = np.array(values)
-        site_labels.append(gene_name)
+        site_labels.append((site_name, uniprot_id, site_position))
         site_rows.append(values)
 
-    prot_arr = np.array(prot_rows[0:100])
-    site_arr = np.array(site_rows[0:50])
+    # Then get the RNA data
+    rna_labels = []
+    rna_rows = []
+    for row_ix, row in enumerate(read_unicode_csv(rna_filename, skiprows=1,
+                                                  delimiter='\t')):
+        values = []
+        gene_name = row[0]
+        for cell_line in cell_lines:
+            col_ix = rna_col_map[cell_line]
+            val = float(row[col_ix])
+            values.append(val)
+        values = np.array(values)
+        rna_labels.append((gene_name,))
+        rna_rows.append(values)
+
+    prot_arr = np.array(prot_rows)
+    site_arr = np.array(site_rows)
+    rna_arr = np.array(rna_rows)
+
     return {'prot_labels': prot_labels, 'prot_arr': prot_arr,
-            'site_labels': site_labels, 'site_arr': site_arr}
+            'site_labels': site_labels, 'site_arr': site_arr,
+            'rna_labels': rna_labels, 'rna_arr': rna_arr}
+
+
+def get_top_correlations(corrs, site_labels, pred_labels, max_corrs=100):
+    site_dict = {}
+    for row_ix in range(corrs.shape[0]):
+        label = site_labels[row_ix]
+        corr_row = corrs[row_ix,:]
+        sort_ixs = np.argsort(np.abs(corr_row))
+        corr_vec = [(pred_labels[ix], corr_row[ix])
+                    for ix in sort_ixs[:-max_corrs:-1]]
+        site_label = site_labels[row_ix]
+        site_dict[site_label] = corr_vec
+    return site_dict
+
+
+def get_default_corrs(corr_dict, num_features=100):
+    all_corrs = []
+    # Add all the gene names
+    for site, corr_list in corr_dict.items():
+        all_corrs.extend([t[0][0] for t in corr_list if t[0][0]])
+
+    ctr = Counter(all_corrs)
+    ctr = sorted([(k, v) for k, v in ctr.items()], key=lambda x: x[1],
+                 reverse=True)
+    genes = [t[0] for t in ctr[0:num_features]]
+    return genes
+
+
+def build_prior():
+    pass
 
 
 if __name__ == '__main__':
     pms_filename = 'sources/Merged_dataset_normalized_subset.csv'
     ibaq_filename = 'sources/ibaq_normalized.csv'
+    rna_filename = 'sources/RNAseq-rpkm.tsv'
 
-    pms_col_map, ibaq_col_map = get_cell_line_col_maps(pms_filename,
-                                                       ibaq_filename)
-    data = load_data(pms_filename, ibaq_filename)
+    data = load_data(pms_filename, ibaq_filename, rna_filename)
 
     print("Calculating correlation coefficients")
     #scipy.stats.spearmanr(site_arr, prot_arr, nan_policy='omit')
-    stacked_data = np.vstack([data['site_arr'], data['prot_arr']])
-    corr = scipy.stats.spearmanr(stacked_data, nan_policy='omit', axis=1)
-    with open('brca_data.pkl', 'wb') as f:
-        pickle.dump(data, f)
-    np.save('brca_spearman', corr)
+    prot_corr_mat = scipy.stats.spearmanr(
+                    np.vstack([data['site_arr'], data['prot_arr']]),
+                    nan_policy='omit', axis=1)
+    prot_corrs = prot_corr_mat[0][0:len(data['site_arr']),
+                                  len(data['site_arr']):]
+    rna_corr_mat = scipy.stats.spearmanr(
+                    np.vstack([data['site_arr'], data['rna_arr']]),
+                    nan_policy='omit', axis=1)
+    rna_corrs = rna_corr_mat[0][0:len(data['site_arr']),
+                                  len(data['site_arr']):]
+
+    #with open('brca_data.pkl', 'wb') as f:
+    #    pickle.dump(data, f)
+    #np.save('brca_spearman', corr)
+
+    prot_corr_dict = get_top_correlations(prot_corrs,
+                                     data['site_labels'], data['prot_labels'])
+    rna_corr_dict = get_top_correlations(rna_corrs,
+                                     data['site_labels'], data['rna_labels'])
+
+    prot_default = get_default_corrs(prot_corr_dict)
+    rna_default = get_default_corrs(rna_corr_dict)
+
+    prior = build_prior(prot_corr_dict, rna_corr_dict, prot_default,
+                        rna_default)
 
 """
 import sklearn
