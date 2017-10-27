36ff72b7a7d628b70ff9525c3e1295f1d13fac0e
John Bachman <bachmanjohn@gmail.com>
2017-10-27 16:17:23 -0400

    Build prot-level and default phospho priors

--- cptac/sc3/phospho_prior.py
+++ cptac/sc3/phospho_prior.py
@@ -12,6 +12,7 @@ from indra.statements import *
 from indra.db.query_db_stmts import by_gene_role_type
 import synapseclient
 from indra.databases import omnipath_client
+from collections import Counter
 
 def get_ids(hgnc_name):
     hgnc_id = hgnc_client.get_hgnc_id(hgnc_name)
@@ -41,19 +42,35 @@ def load_ov_sites(use_mapped=True):
     return ov_sites
 
 
-def load_annotations_from_synapse(synapse_id='syn10998244'):
+def load_prior_from_synapse(synapse_id='syn11273504', peptide_specific=True):
     syn = synapseclient.Synapse()
     syn.login()
     # Obtain a pointer and download the data
-    syn_data = syn.get('syn10998244')
+    syn_data = syn.get(synapse_id)
     prior = {}
+    for row in read_unicode_csv(syn_data.path, delimiter='\t'):
+        sub_name, site_info = row[0].split(':')
+        res = site_info[0]
+        pos = site_info[1:]
+        gene_list = row[1].split(',')
+        if peptide_specific:
+            prior[site_info] = gene_list
+        else:
+            prior[sub_name] = gene_list
+    return prior
+
+
+def load_statements_from_synapse(synapse_id='syn11273504'):
+    syn = synapseclient.Synapse()
+    syn.login()
+    # Obtain a pointer and download the data
+    syn_data = syn.get(synapse_id)
     stmts = []
     for row in read_unicode_csv(syn_data.path, delimiter='\t'):
         sub_name, site_info = row[0].split(':')
         res = site_info[0]
         pos = site_info[1:]
         gene_list = row[1].split(',')
-        prior[site_info] = gene_list
         for enz_name in gene_list:
             enz = Agent(enz_name, db_refs=get_ids(enz_name))
             sub = Agent(sub_name, db_refs=get_ids(sub_name))
@@ -61,7 +78,7 @@ def load_annotations_from_synapse(synapse_id='syn10998244'):
             stmts.append(stmt)
     stmts = ac.map_sequence(stmts)
     stmts = ac.filter_human_only(stmts)
-    #stmts = ac.filter_genes_only(stmts, specific_only=True)
+    stmts = ac.filter_genes_only(stmts, specific_only=True)
     return stmts
 
 
@@ -78,16 +95,17 @@ def get_omnipath_stmts():
 
 def get_indra_phos_stmts():
     stmts = by_gene_role_type(stmt_type='Phosphorylation')
+    stmts += by_gene_role_type(stmt_type='Dephosphorylation')
     stmts = ac.map_grounding(stmts)
     # Expand families before site mapping
     stmts = ac.expand_families(stmts)
     stmts = ac.filter_grounded_only(stmts)
     stmts = ac.map_sequence(stmts)
+    ac.dump_statements(stmts, 'sources/indra_phos_sitemap.pkl')
     stmts = ac.run_preassembly(stmts, poolsize=4,
                                save='sources/indra_phos_stmts_pre.pkl')
     stmts = ac.filter_human_only(stmts)
     stmts = ac.filter_genes_only(stmts, specific_only=True)
-    stmts = [s for s in stmts if s.enz and s.sub and s.residue and s.position]
     ac.dump_statements(stmts, 'sources/indra_phos_stmts.pkl')
     return stmts
 
@@ -200,6 +218,20 @@ def to_prior(stmts):
     return prior
 
 
+def to_nonspec_prior(stmts):
+    prior = {}
+    for stmt in stmts:
+        if stmt.enz is None or stmt.sub is None:
+            continue
+        enz = stmt.enz.name
+        sub = stmt.sub.name
+        if sub not in prior:
+            prior[sub] = set([enz])
+        else:
+            prior[sub].add(enz)
+    return prior
+
+
 def save_prior(prior, prior_name):
     with open(prior_name, 'wt') as f:
         for gene_key in sorted(prior.keys()):
@@ -241,16 +273,57 @@ def get_brca_corr_prior(site_list):
         prior[site_key] = genes
     return prior
 
+
+def load_pc_phos():
+    site_info = {}
+    stmts = []
+    for row in read_unicode_csv('sources/PathwayCommons9.All.hgnc.sif',
+                                delimiter='\t'):
+        relation = row[1]
+        if relation != 'controls-phosphorylation-of':
+            continue
+        enzyme = row[0]
+        enz_agent = Agent(enzyme, db_refs={'HGNC': enzyme})
+        substrate = row[2]
+        sub_agent = Agent(substrate, db_refs={'HGNC': substrate})
+        stmt = Phosphorylation(enz_agent, sub_agent)
+        stmts.append(stmt)
+    return stmts
+
+
+def save_gene_prior(prior, filename):
+    site_prior = {}
+    for row in read_unicode_csv('mapped_peptides.txt', delimiter='\t',
+                                skiprows=1):
+        # Check for uniprot ID
+        site_id = row[0]
+        gene_name = row[2]
+        if gene_name in prior:
+            site_prior[site_id] = prior[gene_name]
+    with open(filename, 'wt') as f:
+        for site_id, gene_list in site_prior.items():
+            f.write('%s\t%s\n' % (site_id, ','.join(gene_list)))
+
+
+def save_default_prior(gene_list, filename):
+    with open(filename, 'wt') as f:
+        for row in read_unicode_csv('mapped_peptides.txt', delimiter='\t',
+                                    skiprows=1):
+            # Check for uniprot ID
+            site_id = row[0]
+            f.write('%s\t%s\n' % (site_id, ','.join(gene_list)))
+
+
 if __name__ == '__main__':
-    ov_sites = load_ov_sites(use_mapped=True)
-    brca_prior = get_brca_corr_prior(ov_sites)
-    save_prior(brca_prior, 'brca_prot_corr_100_prior.txt')
+    #ov_sites = load_ov_sites(use_mapped=True)
+    #brca_prior = get_brca_corr_prior(ov_sites)
+    #save_prior(brca_prior, 'brca_prot_corr_100_prior.txt')
 
-    import sys
-    sys.exit()
+    #brca_sites = load_brca_sites()
+    #print("BRCA phospho-MS data: %d of %d peptides" %
+    #      (coverage(ov_sites, brca_sites), len(ov_sites)))
 
-    brca_sites = load_brca_sites()
-
+    """
     reg_stmts = get_indra_reg_act_stmts()
     act_stmts = ac.filter_by_type(reg_stmts, Activation)
     inh_stmts = ac.filter_by_type(reg_stmts, Inhibition)
@@ -258,29 +331,55 @@ if __name__ == '__main__':
     reg_stmts = [s for s in reg_stmts if s.subj is not None]
     reg_stmts = ac.filter_genes_only(reg_stmts, specific_only=True)
     """
-    syn_stmts = load_annotations_from_synapse(synapse_id='syn10998244')
+
+    #indra_stmts = get_indra_phos_stmts()
+    """
+    indra_stmts = ac.load_statements('sources/indra_phos_stmts.pkl')
+    syn_stmts = load_statements_from_synapse(synapse_id='syn10998244')
+    pc_stmts = load_pc_phos()
     omni_stmts = get_omnipath_stmts()
     phos_stmts = get_phosphosite_stmts()
-    #indra_stmts = get_indra_db_stmts()
-    indra_stmts = ac.load_statements('sources/indra_phos_stmts.pkl')
-    indra_stmts = ac.filter_genes_only(indra_stmts)
+    all_stmts = syn_stmts + omni_stmts + phos_stmts + indra_stmts + pc_stmts
+    ac.dump_statements(all_stmts, 'sources/all_stmts.pkl')
     """
+    all_stmts = ac.load_statements('sources/all_stmts.pkl')
+    nsprior = to_nonspec_prior(all_stmts)
+    nsprior_filename = 'priors/indra_nkconf2_combined_prot_spec.txt'
+    save_gene_prior(nsprior, nsprior_filename)
+    syn = synapseclient.login()
+    syn_file = synapseclient.File(nsprior_filename, parent='syn11272284')
+    syn.store(syn_file)
 
+    all_kinases = [k for kin_list in nsprior.values()
+                     for k in kin_list]
+    kin_ctr = Counter(all_kinases)
+    kin_ctr = sorted([(k, v) for k, v in kin_ctr.items()],
+                     key=lambda x: x[1], reverse=True)
+
+    default_prior_list = [t[0] for t in kin_ctr[0:200]]
+    default_prior_filename = 'priors/indra_nkconf2_combined_default200.txt'
+    save_default_prior(default_prior_list, default_prior_filename)
+    syn_file = synapseclient.File(default_prior_filename, parent='syn11272284')
+    syn.store(syn_file)
+
+    import sys
+    sys.exit()
+
+    """
     with open('sources/stmt_cache.pkl', 'rb') as f:
         syn_stmts, omni_stmts, phos_stmts, indra_stmts = pickle.load(f)
+    """
 
-    db_stmts = syn_stmts + omni_stmts + phos_stmts
-    all_stmts = syn_stmts + omni_stmts + phos_stmts + indra_stmts
+    #db_stmts = syn_stmts + omni_stmts + phos_stmts
     all_stmts = ac.filter_genes_only(all_stmts, specific_only=True)
 
     all_prior = to_prior(all_stmts)
+
     ext_prior_100 = add_regulators(reg_stmts, all_prior, max_features=100)
     save_prior(ext_prior_100, 'regulators_prior_100.txt')
     ext_prior_200 = add_regulators(reg_stmts, all_prior, max_features=200)
     save_prior(ext_prior_200, 'regulators_prior_200.txt')
 
-    import sys
-    sys.exit()
 
     print("Phosphosite: %d of %d peptides" %
           (coverage(ov_sites, get_stmt_sites(phos_stmts)), len(ov_sites)))
