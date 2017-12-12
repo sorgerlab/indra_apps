49af3aff9434c02df17501709b3faa97d0589e14
John Bachman <bachmanjohn@gmail.com>
2017-12-12 17:29:55 -0500

    Script to check for causal paths in model

---
+++ dyn_model/check_model.py
@@ -0,0 +1,13 @@
+import pickle
+from indra.sources import trips
+from indra.explanation.model_checker import ModelChecker
+from util import pklload
+
+pysb_model = pklload('pysb_model')
+pysb_stmts = pklload('pysb_stmts')
+
+#stmts_to_check = trips.process_text('BRAF phosphorylates MAPK1.').statements
+stmts_to_check = trips.process_text('HRAS phosphorylates MAPK1.').statements
+
+mc = ModelChecker(pysb_model, stmts_to_check)
+#paths = mc.check_model(max_paths=100, max_path_length=6)
