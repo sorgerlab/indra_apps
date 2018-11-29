bcbbe84d4c6a11dbb0fb11d9cd3207fbc4a37bab
Ben Gyori <ben.gyori@gmail.com>
2018-11-29 09:23:06 -0500

    Add document metadata in annotations

--- wm_12month/assemble_model.py
+++ wm_12month/assemble_model.py
@@ -1,3 +1,4 @@
+import os
 import glob
 import json
 import indra.tools.assemble_corpus as ac
@@ -15,14 +16,14 @@ def process_eidos():
         ep = eidos.process_json_file(fname)
         for stmt in ep.statements:
             for ev in stmt.evidence:
-                ev.annotations['provenance'][0]['document']['@id'] = fname
+                ev.annotations['provenance'][0]['document']['@id'] = \
+                    os.path.basename(fname)
             stmts.append(stmt)
     return stmts
 
 
 def process_hume():
-    path = 'docs/hume/expts/wm_m12.v6.full.v2/serialization/analytic/' + \
-        'wm_m12.v6.full.v2.json-ld'
+    path = 'docs/hume/wm_m12.v8.full.v4.json-ld'
     hp = hume.process_jsonld_file(path)
     return hp.statements
 
@@ -30,6 +31,10 @@ def process_hume():
 def process_sofia():
     fname = 'docs/sofia/MITRE_AnnualEval_v1.xlsx'
     sp = sofia.process_table(fname)
+    for stmt in sp.statements:
+        for ev in stmt.evidence:
+            prov = [{'document': {'@id': ev.pmid}}]
+            ev.annotations['provenance'] = prov
     return sp.statements
 
 
@@ -40,6 +45,10 @@ def process_cwms():
     for ekb in ekbs:
         cp = cwms.process_ekb_file(ekb)
         stmts += cp.statements
+    for stmt in stmts:
+        for ev in stmt.evidence:
+            prov = [{'document': {'@id': ev.pmid}}]
+            ev.annotations['provenance'] = prov
     return stmts
 
 
@@ -50,6 +59,7 @@ def assemble_stmts(stmts):
     stmts = ac.run_preassembly(stmts, belief_scorer=scorer,
                                return_toplevel=False)
     return stmts
+
 
 def dump_stmts_json(stmts, fname):
     jd = stmts_to_json(stmts, use_sbo=False)
@@ -64,4 +74,4 @@ if __name__ == '__main__':
     sofia_stmts = process_sofia()
     stmts = hume_stmts + eidos_stmts + cwms_stmts + sofia_stmts
     stmts = assemble_stmts(stmts)
-    dump_stmts_json(stmts, 'wm_12_month_4_reader_20181128.json')
+    dump_stmts_json(stmts, 'wm_12_month_4_reader_20181128_v2.json')
