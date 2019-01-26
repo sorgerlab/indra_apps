9a3e459a8af142fe3d4bf5df0dd06429e3acac97
John Bachman <bachmanjohn@gmail.com>
2019-01-25 23:35:47 -0500

    Get stmt counts for each gene from db_dump

--- stmt_count/stmt_count.py
+++ stmt_count/stmt_count.py
@@ -5,7 +5,7 @@ import pickle
 from indra.util import read_unicode_csv
 from indra_db.util import get_primary_db
 
-def get_stmt_count_from_db():
+def get_hgnc_entries():
     # Get all HGNC IDs
     hgnc_file = '../../indra/indra/resources/hgnc_entries.tsv'
     lines = read_unicode_csv(hgnc_file, delimiter='\t')
@@ -13,6 +13,10 @@ def get_stmt_count_from_db():
     next(lines)
     hgnc_entries = [(line[1], line[0].split(':')[1])
                     for line in lines if line[3] == 'Approved']
+    return hgnc_entries
+
+def get_stmt_count_from_db():
+    hgnc_entries = get_hgnc_entries()
     random.seed(1)
     random.shuffle(hgnc_entries)
 
@@ -63,4 +67,51 @@ def get_stmt_count_from_db():
     return stmt_counts
 
 
+def get_stmt_count_from_stmt_df(df):
+    # Get statements with gene in subject position
+    hgnc_entries = get_hgnc_entries()
+    # Load statement dataframe
+    with open('../../mek_crispr/networks/stmt_df.pkl', 'rb') as f:
+        df = pickle.load(f)
+    df = df[['agA_ns', 'agA_id', 'agB_ns', 'agB_id', 'evidence_count']]
+    df_hgncA = df[df['agA_ns'] == 'HGNC']
+    df_hgncB = df[df['agB_ns'] == 'HGNC']
+    df_hgncAB = df[(df['agA_ns'] == 'HGNC') & (df['agB_ns'] == 'HGNC')]
 
+    stmt_counts = {}
+    for hgnc_name, hgnc_id in hgnc_entries:
+        a_count = df_hgncA[df_hgncA['agA_id'] == 
+                                    hgnc_id]['evidence_count'].sum()
+        b_count = df_hgncB[df_hgncB['agB_id'] == 
+                                    hgnc_id]['evidence_count'].sum()
+        ab_count = df_hgncAB[(df_hgncAB['agA_id'] == hgnc_id) &
+                             (df_hgncAB['agB_id'] == hgnc_id)]\
+                                            ['evidence_count'].sum()
+        total_count = a_count + b_count - ab_count
+        print(hgnc_name, total_count)
+        stmt_counts[hgnc_name] = total_count
+    return stmt_counts
+
+
+def get_stmt_count_from_stmt_df():
+    with open('../../mek_crispr/networks/stmt_df.pkl', 'rb') as f:
+        df = pickle.load(f)
+    df_A = df[df['agA_ns'] == 'HGNC'].groupby('agA_id').agg(
+            {'evidence_count': 'sum'})
+    df_B = df[df['agB_ns'] == 'HGNC'].groupby('agB_id').agg(
+                                        {'evidence_count': 'sum'})
+    df_AB = df[(df['agA_ns'] == 'HGNC') &
+               (df['agB_ns'] == 'HGNC') &
+               (df['agA_id'] == df['agB_id'])].groupby('agA_id').agg(
+                                        {'evidence_count': 'sum'})
+    # Combine the counts
+    df_join = df_A.join(df_B, lsuffix='A', rsuffix='B').join(df_AB)
+    df_join = df_join.fillna(value=0)
+    stmt_counts = (df_join['evidence_countA'] +
+                   df_join['evidence_countB'] -
+                   df_join['evidence_count'])
+
+    globals().update(locals())
+
+if __name__ == '__main__':
+    get_stmt_count_from_stmt_df()
