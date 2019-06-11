047ee5815c1768b9d26e8996effc40c335663122
Ben Gyori <ben.gyori@gmail.com>
2019-06-11 11:53:00 -0700

    Generalize assembly functions to influences

--- wm_18months/process.py
+++ wm_18months/process.py
@@ -41,42 +41,82 @@ if __name__ == '__main__':
             return False
         return True
 
-    def event_location_matches(stmt):
-        if isinstance(stmt, Event):
-            if not has_location(stmt):
-                context_key = None
-            else:
-                context_key = stmt.context.geo_location.db_refs['GEOID']
+    def get_location(stmt):
+        if not has_location(stmt):
+            loc = None
+        else:
+            loc = stmt.context.geo_location.db_refs['GEOID']
+        return loc
 
+    def get_time(stmt):
+        if not has_time(stmt):
+            time = None
+        else:
+            time = stmt.context.time
+        return time
+
+    def location_matches(stmt):
+        if isinstance(stmt, Event):
+            context_key = get_location(stmt)
             matches_key = str((stmt.concept.matches_key(), context_key))
+        if isinstance(stmt, Influence):
+            subj_context_key = get_location(stmt.subj)
+            obj_context_key = get_location(stmt.obj)
+            matches_key = str(stmt.matches_key(), subj_context_key,
+                              obj_context_key)
         else:
             matches_key = stmt.matches_key()
         return matches_key
 
     def event_location_refinement(st1, st2, hierarchies):
-        if isinstance(st1, Event) and isinstance(st2, Event):
-            ref = st1.refinement_of(st2, hierarchies)
-            if not ref:
-                return False
-            if not has_location(st2):
-                return True
-            elif not has_location(st1):
-                return False
-            else:
-                return st1.context.geo_location.db_refs['GEOID'] == \
-                       st2.context.geo_location.db_refs['GEOID']
+        ref = st1.refinement_of(st2, hierarchies)
+        if not ref:
+            return False
+        if not has_location(st2):
+            return True
+        elif not has_location(st1):
+            return False
+        else:
+            return st1.context.geo_location.db_refs['GEOID'] == \
+                   st2.context.geo_location.db_refs['GEOID']
 
-    def event_location_time_matches(stmt):
-        mk = event_location_matches(stmt)
-        if not has_time(stmt):
+    def location_refinement(st1, st2, hierarchies):
+        type(st1) != type(st2):
+            return False
+        if isinstance(st1, Event):
+            event_ref = event_location_refinement(st1, st2, hierarchies)
+            return event_ref
+        elif isinstance(st1, Influence):
+            subj_ref = event_location_refinement(st1.subj, st2.subj,
+                                                 hierarchies)
+            obj_ref = event_location_refinement(st1.obj, st2.obj,
+                                                hierarchies)
+            return subj_ref and obj_ref
+        else:
+            return st1.refinement_of(st2, hierarchies)
+
+
+    def event_location_time_matches(event):
+        mk = location_matches(event)
+        if not has_time(event):
             return mk
-        matches_key = str((mk, stmt.context.time.start,
-                           stmt.context.time.end,
-                           stmt.context.time.duration))
+        time = get_time(stmt)
+        matches_key = str((mk, time.start, time.end, time.duration))
         return matches_key
 
+
+    def location_time_matches(stmt):
+        if isinstance(stmt, Event):
+            return event_location_time_matches(stmt)
+        elif isinstance(stmt, Influence):
+            subj_mk = event_location_time_matches(stmt.subj)
+            obj_mk = event_location_time_matches(stmt.obj)
+            return str((stmt.matches_key(), subj_mk, obj_mk))
+        else:
+            return stmt.matches_key()
+
     def event_location_time_refinement(st1, st2, hierarchies):
-        ref = event_location_refinement(st1, st2, hierarchies)
+        ref = location_refinement(st1, st2, hierarchies)
         if not ref:
             return False
         if not has_time(st2):
@@ -86,11 +126,24 @@ if __name__ == '__main__':
         else:
             return st1.context.time.refinement_of(st2.context.time)
 
-
+    def location_time_refinement(st1, st2, hierarchies):
+        if type(st1) != type(st2):
+            return False
+        if isinstance(st1, Event):
+            return event_location_time_refinement(st1, st2, hierarchies)
+        elif isinstance(st1, Influence):
+            ref = st1.refinement_of(st2, hierarchies)
+            if not ref:
+                return False
+            subj_ref = event_location_time_refinement(st1.subj, st2.subj,
+                                                      hierarchies)
+            obj_ref = event_location_time_refinement(st1.obj, st2.obj,
+                                                     hierarchies)
+            return subj_ref and obj_ref
     funs = {
         'grounding': (None, None),
-        'location': (event_location_matches, event_location_refinement),
-        'location_and_time': (event_location_time_matches,
+        'location': (location_matches, location_refinement),
+        'location_and_time': (location_time_matches,
                               event_location_time_refinement)
         }
 
