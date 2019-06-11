4df4db6ab6e43a89b153eb411bb8990e9339217b
Ben Gyori <ben.gyori@gmail.com>
2019-06-11 11:30:23 -0700

    Assemble with multiple refinement levels

--- wm_18months/process.py
+++ wm_18months/process.py
@@ -36,6 +36,11 @@ if __name__ == '__main__':
             return False
         return True
 
+    def has_time(stmt):
+        if not stmt.context or not stmt.context.time:
+            return False
+        return True
+
     def event_location_matches(stmt):
         if isinstance(stmt, Event):
             if not has_location(stmt):
@@ -55,20 +60,49 @@ if __name__ == '__main__':
                 return False
             if not has_location(st2):
                 return True
-            elif not has_location(st1) and has_location(st2):
+            elif not has_location(st1):
                 return False
             else:
                 return st1.context.geo_location.db_refs['GEOID'] == \
                        st2.context.geo_location.db_refs['GEOID']
 
-    assembled_stmts = ac.run_preassembly(stmts, belief_scorer=scorer)
-    assembled_events = ac.run_preassembly(events, belief_scorer=scorer,
-                                    matches_fun=event_location_matches,
-                                    refinement_fun=event_location_refinement)
-    sj = stmts_to_json(assembled_stmts)
-    with open('jsonld-merged20190607-stmts.json', 'w') as fh:
-        json.dump(sj, fh, indent=1)
+    def event_location_time_matches(stmt):
+        mk = event_location_matches(stmt)
+        if not has_time(stmt):
+            return mk
+        matches_key = str((mk, stmt.context.time.start,
+                           stmt.context.time.end,
+                           stmt.context.time.duration))
+        return matches_key
 
-    ej = stmts_to_json(assembled_events)
-    with open('jsonld-merged20190607-events.json', 'w') as fh:
-        json.dump(ej, fh, indent=1)
+    def event_location_time_refinement(st1, st2, hierarchies):
+        ref = event_location_refinement(st1, st2, hierarchies)
+        if not ref:
+            return False
+        if not has_time(st2):
+            return True
+        elif not has_time(st1) :
+            return False
+        else:
+            return st1.context.time.refinement_of(st2.context.time)
+
+
+    funs = {
+        'grounding': (None, None),
+        'location': (event_location_matches, event_location_refinement),
+        'location_and_time': (event_location_time_matches,
+                              event_location_time_refinement)
+        }
+
+    for key, (matches_fun, refinement_fun) in funs.items():
+        assembled_stmts = ac.run_preassembly(stmts, belief_scorer=scorer)
+        assembled_events = ac.run_preassembly(events, belief_scorer=scorer,
+                                    matches_fun=matches_fun,
+                                    refinement_fun=refinement_fun)
+        sj = stmts_to_json(assembled_stmts)
+        with open('jsonld-merged20190609-stmts-%s.json' % key, 'w') as fh:
+            json.dump(sj, fh, indent=1)
+
+        ej = stmts_to_json(assembled_events)
+        with open('jsonld-merged20190609-events-%s.json' % key, 'w') as fh:
+            json.dump(ej, fh, indent=1)
