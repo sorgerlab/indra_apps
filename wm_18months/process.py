b997f612eb97e5523fe0a19137f88add627099e7
Ben Gyori <ben.gyori@gmail.com>
2019-06-27 15:46:37 -0400

    Refine assembly script for Influence/Event assembly

--- wm_18months/process.py
+++ wm_18months/process.py
@@ -1,5 +1,6 @@
 import json
 import glob
+import copy
 from indra.sources import eidos
 from indra.tools import assemble_corpus as ac
 from indra.belief.wm_scorer import get_eidos_scorer
@@ -7,13 +8,7 @@ from indra.preassembler.custom_preassembly import *
 
 fnames = glob.glob('jsonld-merged20190404/*.jsonld')
 
-
-if __name__ == '__main__':
-    stmts = []
-    for fname in fnames:
-        ep = eidos.process_json_file(fname)
-        stmts += ep.statements
-
+def get_events(stmts):
     # Create list of standalone events
     events = []
     for stmt in stmts:
@@ -27,7 +22,43 @@ if __name__ == '__main__':
                 events.append(member)
         elif isinstance(stmt, Event):
             events.append(stmt)
+    return events
 
+
+def get_non_events(stmts):
+    return [st for st in stmts if not isinstance(st, Event)]
+
+
+def remove_namespaces(stmts, namespaces):
+    for stmt in stmts:
+        for agent in stmt.agent_list():
+            for namespace in namespaces:
+                if namespace in copy.deepcopy(agent.db_refs):
+                    agent.db_refs.pop(namespace, None)
+
+
+def remove_raw_grounding(stmts):
+    for stmt in stmts:
+        for ev in stmt.evidence:
+            if not ev.annotations:
+                continue
+            agents = ev.annotations.get('agents')
+            if not agents:
+                continue
+            if 'raw_grounding' in agents:
+                agents.pop('raw_grounding', None)
+
+
+
+if __name__ == '__main__':
+    stmts = []
+    for fname in fnames:
+        ep = eidos.process_json_file(fname)
+        stmts += ep.statements
+    remove_namespaces(stmts, ['WHO', 'MITRE12'])
+
+    events = get_events(stmts)
+    non_events = get_non_events(stmts)
     scorer = get_eidos_scorer()
 
     funs = {
@@ -38,14 +69,15 @@ if __name__ == '__main__':
         }
 
     for key, (matches_fun, refinement_fun) in funs.items():
-        assembled_stmts = ac.run_preassembly(stmts, belief_scorer=scorer)
+        assembled_non_events = ac.run_preassembly(non_events,
+                                                  belief_scorer=scorer,
+                                                  matches_fun=matches_fun,
+                                                  refinement_fun=refinement_fun)
         assembled_events = ac.run_preassembly(events, belief_scorer=scorer,
                                     matches_fun=matches_fun,
                                     refinement_fun=refinement_fun)
+        assembled_stmts = assembled_non_events + assembled_events
+        remove_raw_grounding(assembled_stmts)
         sj = stmts_to_json(assembled_stmts)
-        with open('jsonld-merged20190611-stmts-%s.json' % key, 'w') as fh:
+        with open('jsonld-merged20190627-stmts-%s.json' % key, 'w') as fh:
             json.dump(sj, fh, indent=1)
-
-        ej = stmts_to_json(assembled_events)
-        with open('jsonld-merged20190611-events-%s.json' % key, 'w') as fh:
-            json.dump(ej, fh, indent=1)
