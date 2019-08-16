04d7b6ec681ec6c2b9fa113fe125a9b6e04ee5f0
Ben Gyori <ben.gyori@gmail.com>
2019-08-16 12:44:46 -0400

    Add two CLI calls to get statements and run Genewalk

---
+++ melanoma_genewalk/run_genewalk.sh
@@ -0,0 +1,1 @@
+python -m genewalk.cli --project melanoma --genes ../indra_apps/melanoma_genewalk/A375_Day4_de_genes.txt --id_type hgnc_id --network_source indra --network_file melanoma_genewalk_stmts.pkl
