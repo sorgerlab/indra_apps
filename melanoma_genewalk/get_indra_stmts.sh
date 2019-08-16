34dec7516229dd49a6f3f72adb71a5876527f1ba
Ben Gyori <ben.gyori@gmail.com>
2019-08-16 16:02:09 -0400

    Rename script and update command line scripts

--- melanoma_genewalk/get_indra_stmts.sh
+++ melanoma_genewalk/get_indra_stmts.sh
@@ -1,1 +1,1 @@
-python -m genewalk.get_indra_stmts --df ../../genewalk/data/refresh_db_dataframe_20190702.pkl --genes A375_Day4_de_genes.txt --stmts melanoma_genewalk_stmts.pkl
+python -m genewalk.get_indra_stmts --df ../../genewalk/data/refresh_db_dataframe_20190702.pkl --genes A375_Day4_de_genes.txt --stmts A375_Day4_stmts.pkl
--- melanoma_genewalk/run_genewalk.sh
+++ melanoma_genewalk/run_genewalk.sh
@@ -1,1 +1,1 @@
-python -m genewalk.cli --project melanoma --genes ../indra_apps/melanoma_genewalk/A375_Day4_de_genes.txt --id_type hgnc_id --network_source indra --network_file melanoma_genewalk_stmts.pkl
+python -m genewalk.cli --project melanoma --genes A375_Day4_de_genes.txt --id_type hgnc_id --network_source indra --network_file A375_Day4_stmts.pkl
