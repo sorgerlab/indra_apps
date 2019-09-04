249a55573508579ec979e023359cfd7fd1578fa0
dianakolusheva <dianakolusheva@gmail.com>
2019-09-04 16:37:00 -0400

    Add some assembly steps

--- wm_migration/process_spreadsheet.py
+++ wm_migration/process_spreadsheet.py
@@ -1,7 +1,19 @@
 from indra.tools import assemble_corpus as ac
 from indra.sources.eidos import migration_table_processor as mtp
+from indra.preassembler.hierarchy_manager import YamlHierarchyManager
+from indra.preassembler.make_eidos_hume_ontologies import load_yaml_from_url, \
+    rdf_graph_from_yaml
+from indra.belief.wm_scorer import get_eidos_scorer
 
 
 if __name__ == '__main__':
     fname = 'Initial annotation exercise for migration use case.xlsx'
     stmts = mtp.process_workbook(fname)
+    wm_ont_url = ('https://raw.githubusercontent.com/WorldModelers/'
+                  'Ontologies/migration_edits/wm.yml')
+    hm = YamlHierarchyManager(load_yaml_from_url(wm_ont_url),
+                              rdf_graph_from_yaml, True)
+    stmts = ac.run_preassembly(stmts, return_toplevel=False,
+                               belief_score=get_eidos_scorer(),
+                               hierarchies={'entity': hm})
+    stmts = ac.standardize_names_groundings(stmts)
