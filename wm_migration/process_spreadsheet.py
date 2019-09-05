a27a8fb5f3c478b63d4ec93c76cdfd05e32674a6
dianakolusheva <dianakolusheva@gmail.com>
2019-09-05 14:18:39 -0400

    Add new matches_key and refinement functions

--- wm_migration/process_spreadsheet.py
+++ wm_migration/process_spreadsheet.py
@@ -3,6 +3,8 @@ from indra.sources.eidos import migration_table_processor as mtp
 from indra.preassembler.hierarchy_manager import YamlHierarchyManager
 from indra.preassembler.make_eidos_hume_ontologies import load_yaml_from_url, \
     rdf_graph_from_yaml
+from indra.preassembler.custom_preassembly import location_time_delta_matches, \
+    location_time_delta_refinement
 from indra.belief.wm_scorer import get_eidos_scorer
 
 
@@ -17,5 +19,7 @@ if __name__ == '__main__':
                               rdf_graph_from_yaml, True)
     stmts = ac.run_preassembly(stmts, return_toplevel=False,
                                belief_score=get_eidos_scorer(),
-                               hierarchies={'entity': hm})
+                               hierarchies={'entity': hm},
+                               matches_fun=location_time_delta_matches,
+                               refinement_fun=location_time_delta_refinement)
     stmts = ac.standardize_names_groundings(stmts)
