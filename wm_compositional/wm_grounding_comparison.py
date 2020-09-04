6814a2740785f0c05880b163d2d65c22dd39720b
zupon <zupon@email.arizona.edu>
2020-09-04 15:52:14 -0700

    added placeholder for compositional INDRA statements

--- wm_compositional/wm_grounding_comparison.py
+++ wm_compositional/wm_grounding_comparison.py
@@ -5,24 +5,40 @@ comparing the flat and compositional grounders.
 import csv
 from indra.statements import stmts_from_json_file
 
-stmts = stmts_from_json_file('statements.json', format='jsonl')
+# INDRA statements from flat ontology
+stmts_flat = stmts_from_json_file('statements.json', format='jsonl')
 
-def make_comparison_sheet(statements: list):
-    header = ["id", "Evidence", "Subject Text", "Subject Groundings", "Object Text", "Object Groundings"]
+# does not exist yet!
+stmts_comp = stmts_from_json_file('statements_comp.json', format='jsonl')
+
+def make_comparison_sheet(flat_statements, comp_statements):
+    header = ["Entity Text", "Grounding", "Confidence"]
     with open('wm_comparison_sheet.tsv', 'wt') as out_file:
         tsv_writer = csv.writer(out_file, delimiter='\t')
         tsv_writer.writerow(header)
-        i = 1
-        for statement in statements:
-            belief = statement.belief
-            evidences = statement.evidence
-            for evidence in evidences:
-                evidence_text = evidence.text
-                subject_text = subj_from_evidence(evidence)
-                object_text = obj_from_evidence(evidence)
-                line = [i, evidence_text, subject_text, groundings_from_event(statement.subj), object_text, groundings_from_event(statement.obj)]
-                tsv_writer.writerow(line)
-                i += 1
+        for statement in flat_statements[:500]:
+            # get text/groundings for flat subject/object
+            flat_subj_text = get_entity_text(statement.subj)
+            flat_subj_grounding = get_groundings(statement.subj)[0]
+            flat_obj_text = get_entity_text(statement.obj)
+            flat_obj_grounding = get_groundings(statement.obj)[0]
+            for statement in comp_statements[:500]:
+                # get text/groundings for compositional subject/object
+                # TODO: get into compositional tuples!
+                comp_subj_text = get_entity_text(statement.subj)
+                (comp_subj_groundings, comp_subj_scores) = get_compositional_groundings(statement.subj)
+                comp_obj_text = get_entity_text(statement.obj)
+                (comp_obj_groundings, comp_obj_scores) = get_compositional_groundings(statement.obj)
+
+                if (flat_subj_text == comp_subj_text && flat_obj_text == comp_obj_text):
+                    flat_subj_line = [flat_subj_text, flat_subj_grounding[0], flat_subj_grounding[1]]
+                    comp_subj_line = [comp_subj_text, comp_subj_groundings, comp_subj_scores]
+                    flat_obj_line = [flat_obj_text, flat_obj_grounding[0], flat_obj_grounding[1]]
+                    comp_obj_line = [comp_obj_text, comp_obj_groundings, comp_obj_scores]
+                    tsv_writer.writerow(flat_subj_line)
+                    tsv_writer.writerow(comp_subj_line)
+                    tsv_writer.writerow(flat_obj_line)
+                    tsv_writer.writerow(comp_obj_line)
 
 
 def subj_from_evidence(evidence) -> str:
@@ -44,10 +60,26 @@ def get_entity_text(event):
     return entity_text
 
 
-def groundings_from_event(event):
+def get_groundings(event):
     """Returns a list of (grounding, confidence) tuples from an event."""
     db_refs = event.to_json()["concept"]["db_refs"]
     groundings = db_refs["WM"]
     return groundings
 
-make_comparison_sheet(stmts)
+
+def get_compositional_groundings(event):
+    """Returns a list of (grounding, confidence) tuples from an event."""
+    db_refs = event.to_json()["concept"]["db_refs"]
+    grounding = db_refs["WM"][0]
+    terms = []
+    scores = []
+    for item in grounding:
+        if item != None:
+            term = item[0]
+            score = item[1]
+            terms.append(term)
+            scores.append(score)
+    return (terms, scores)
+
+
+make_comparison_sheet(stmts_flat)
